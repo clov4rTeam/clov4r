f70b0f66ef3b678063cece8e9fce1322f141de6d
李广华 <yykxlgh@126.com>
2014-10-29 14:02:10 +0800

    登录

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,5 +12,22 @@ public class Application extends Controller {
     public static void index() {
         render();
     }
+    public static void login() {
+        render();
+    }
+    public static void loginIn(String username, String password) {
+    	System.out.println(username+"      "+password);
+    	
+    	User user = User.find("byUsernameAndPassword", username,password).first();
+    	if(user != null)
+    	{
+            session.put("username", username);
+            System.out.println(username);
+            index();
+    	}
 
+        params.flash();
+        flash.error("用户名或密码错误");
+        login();
+    }
 }
--- app/models/User.java
+++ app/models/User.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -9,4 +11,18 @@ import play.db.jpa.Model;
 @Table(name="user")
 public class User extends Model {
 
+	//用户名
+	public String username;
+	//密码
+	public String password;
+	
+	//真实姓名
+	public String realUserName;
+	//电话
+	public String mobile;
+	
+	
+	//邮箱
+	public String email;
+	public Date createDate;
 }
